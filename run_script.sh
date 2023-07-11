e60b65edc830a558490734f56106084abaa393ba
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-11 16:22:03 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install net-tools -y
-sudo apt install terminator
+sudo apt install terminator -y
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
