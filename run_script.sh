3feccd47aec03375242ccd1b8acd1b86dfe50f7f
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 10:48:21 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -24,15 +24,15 @@ mkdir -p $HOME/catkin_ws/src
 sudo apt install ros-noetic-image-proc -y
 sudo apt install ros-noetic-aruco-ros -y
 
+#Install FLIR camera ROS dependencies
+sudo apt install ros-noetic-camera-info-manager ros-noetic-diagnostic-updater ros-noetic-dynamic-reconfigure ros-noetic-image-exposure-msgs ros-noetic-image-transport ros-noetic-nodelet ros-noetic-roscpp ros-noetic-sensor-msgs ros-noetic-wfov-camera-msgs -y
+
 #Download and Install FLIR camera SDK, and InertialLabs ROS packages from my GIT Repo
 (cd $HOME/Downloads && git clone https://github.com/petartrs/guard.git)
 (cd $HOME/Downloads/guard/spinnaker-3.1.0.79-amd64 && sudo sh install_spinnaker.sh)
 sudo cp -a guard/flir_camera_driver-ros-pkgs/ $HOME/catkin_ws/src
 sudo cp -a guard/inertiallabs-ros-pkgs/ $HOME/catkin_ws/src
 sudo cp -a guard/aruco-ros-pkgs/ $HOME/catkin_ws/src
-
-#Install FLIR camera dependencies
-sudo apt install ros-noetic-camera-info-manager ros-noetic-diagnostic-updater ros-noetic-dynamic-reconfigure ros-noetic-image-exposure-msgs ros-noetic-image-transport ros-noetic-nodelet ros-noetic-roscpp ros-noetic-sensor-msgs ros-noetic-wfov-camera-msgs -y
 
 (cd $HOME/catkin_ws && catkin_make)
 (cd $HOME/catkin_ws && catkin_make_isolated)
