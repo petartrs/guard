277e59c420c2ff9e2cf91e4bf20136c5ac21ce0c
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 12:24:01 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -2,7 +2,7 @@
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
