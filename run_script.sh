9b5048918d65d9cd0a590940c639e643b6fe157c
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 10:26:05 +0100

    Add files via upload

--- run_script.sh
+++ run_script.sh
@@ -2,14 +2,13 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install net-tools -y
-sudo apt install terminator -y
+sudo apt install terminator
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo "source /opt/ros/noetic/setup.bash" >> /home/$USER/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> /home/$USER/.bashrc
-echo "source ~/catkin_ws/devel_isolated/setup.bash" >> /home/$USER/.bashrc
-sudo apt install curl -y
+sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
@@ -35,5 +34,7 @@ sudo cp -a guard/aruco-ros-pkgs/ $HOME/catkin_ws/src
 sudo apt install ros-noetic-camera-info-manager ros-noetic-diagnostic-updater ros-noetic-dynamic-reconfigure ros-noetic-image-exposure-msgs ros-noetic-image-transport ros-noetic-nodelet ros-noetic-roscpp ros-noetic-sensor-msgs ros-noetic-wfov-camera-msgs -y
 
 (cd $HOME/catkin_ws && catkin_make)
+(cd $HOME/catkin_ws && catkin_make_isolated)
+echo "source ~/catkin_ws/devel_isolated/setup.bash" >> /home/$USER/.bashrc
 
 #Install FLIR Camera ROS Driver
