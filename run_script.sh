9b3a288e4103caa987904c9cf5dbfdaa0091019f
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 12:21:29 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt install net-tools -y
-sudo apt install terminator -y
-sudo apt install build-essential gcc make perl dkms -y
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -11,21 +6,27 @@ sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
-sudo rosdep init
-rosdep update
-echo "source /opt/ros/noetic/setup.bash" >> /home/$USER/.bashrc
-echo "source ~/catkin_ws/devel/setup.bash" >> /home/$USER/.bashrc
-source ~/.bashrc
-mkdir -p $HOME/catkin_ws/src
-(cd $HOME/catkin_ws && catkin_make)
 
 #Install standard ROS packages
 sudo apt install ros-noetic-image-proc -y
 sudo apt install ros-noetic-aruco-ros -y
 
-#Install FLIR camera ROS dependencies
+#Install FLIR camera ROS packages/dependencies
 sudo apt install ros-noetic-camera-info-manager ros-noetic-diagnostic-updater ros-noetic-dynamic-reconfigure ros-noetic-image-exposure-msgs ros-noetic-image-transport ros-noetic-nodelet ros-noetic-roscpp ros-noetic-sensor-msgs ros-noetic-wfov-camera-msgs -y
+
+echo "source /opt/ros/noetic/setup.bash" >> /home/$USER/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+
+source /opt/ros/noetic/setup.bash
+mkdir -p $HOME/catkin_ws/src
+(cd $HOME/catkin_ws && catkin_make)
+echo "source ~/catkin_ws/devel/setup.bash" >> /home/$USER/.bashrc
+source devel/setup.bash
 
 #Download and Install FLIR camera SDK, and InertialLabs ROS packages from my GIT Repo
 (cd $HOME/Downloads && git clone https://github.com/petartrs/guard.git)
@@ -38,5 +39,10 @@ source ~/.bashrc
 (cd $HOME/catkin_ws && catkin_make)
 (cd $HOME/catkin_ws && catkin_make_isolated)
 echo "source ~/catkin_ws/devel_isolated/setup.bash" >> /home/$USER/.bashrc
+
+sudo apt upgrade -y
+sudo apt install net-tools -y
+sudo apt install terminator -y
+sudo apt install build-essential gcc make perl dkms -y
 sudo reboot
 #Install FLIR Camera ROS Driver
