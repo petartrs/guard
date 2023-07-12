6a95ba03375024eb9cf2d3d0c59abc1b7eb70105
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 12:01:54 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -13,10 +13,10 @@ sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
-source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 sudo rosdep init
 rosdep update
+source ~/.bashrc
 mkdir -p $HOME/catkin_ws/src
 (cd $HOME/catkin_ws && catkin_make)
 
