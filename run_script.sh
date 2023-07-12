94053238b401e59ff3bade6cc6ad524e40958b1d
petartrs <76850468+petartrs@users.noreply.github.com>
2023-07-12 12:09:13 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -7,8 +7,6 @@ sudo apt install build-essential gcc make perl dkms -y
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-echo "source /opt/ros/noetic/setup.bash" >> /home/$USER/.bashrc
-echo "source ~/catkin_ws/devel/setup.bash" >> /home/$USER/.bashrc
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
@@ -16,6 +14,8 @@ sudo apt install ros-noetic-desktop-full -y
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 sudo rosdep init
 rosdep update
+echo "source /opt/ros/noetic/setup.bash" >> /home/$USER/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> /home/$USER/.bashrc
 source ~/.bashrc
 mkdir -p $HOME/catkin_ws/src
 (cd $HOME/catkin_ws && catkin_make)
