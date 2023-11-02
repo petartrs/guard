d8deff7e163e844b9c2e52e6bca9543c49c9021d
petartrs <76850468+petartrs@users.noreply.github.com>
2023-11-02 10:56:06 +0000

    Update run_script.sh
    Added apt-get update  and apt-get upgrade at the beginning of the script

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
 
 #Install and setup ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
